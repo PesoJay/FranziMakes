d0c7e9e6a5e94dfd044e92cbf0bc7ca535071daa
jakobdanid01 <jakob.deschauer@gmx.com>
2026-05-23 18:12:33 +0200

    Added cormorant font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Cormorant } from "next/font/google";
+import { Cormorant } from "next/font/google";
 import "./globals.css";
 
 const cormorant = Cormorant({
